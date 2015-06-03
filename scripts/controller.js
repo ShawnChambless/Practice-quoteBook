46b044b56a9a41fd1d2a4cde7735bff18331ba4c
Shawn Chambless <shawn.chambless@gmail.com>
2015-06-02 20:17:49 -0600

    Toggle function now works

--- scripts/controller.js
+++ scripts/controller.js
@@ -4,17 +4,31 @@ var app = angular.module('quotes');
 
      $scope.quotes = dataService.getData();
      $scope.quote = {};
-
+     $scope.showAdd = false;
+     $scope.showRemove = false;
      $scope.addQuote = function() {
          var dataObj = {
              text: $scope.text,
              author: $scope.author
          }
          dataService.addData(dataObj);
+         $scope.toggleAdd = true;
          };
 
      $scope.removeQuote = function() {
          dataService.removeData($scope.title);
+         $scope.toggleRemove = true;
+     }
+
+     $scope.toggleShowAdd = function() {
+        $scope.showAdd = !$scope.showAdd;
+        $scope.showRemove = false;
+
+     }
+     $scope.toggleShowRemove = function() {
+         $scope.showRemove = !$scope.showRemove;
+         $scope.showAdd = false;
+
      }
 
  });
